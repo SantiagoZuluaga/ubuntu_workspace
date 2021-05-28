93425b09e16ac77316a7b6a2db7482445bd5d749
Santiago Zuluaga <santiagozh1998@gmail.com>
2021-05-28 16:30:34 -0500

    Saving a backup of workspace in Ubuntu

---
+++ backup.sh
@@ -0,0 +1,15 @@
+sudo apt install gnome-tweaks
+sudo apt install google-chrome-stable
+sudo snap install code --classic
+sudo snap install curl
+sudo apt install git
+curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs
+sudo apt install openjdk-8jdk
+sudo snap install go --classic
+sudo snap install flutter --classic
+sudo snap install android-studio --classic
+sudo snap install postman
+sudo apt install postgresql postgresql-contrib
+sudo apt install pgadmin3
