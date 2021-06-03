7a8ba0250e81d09b9eadb132c49ea0ece168dd44
Santiago Zuluaga <santiagozh1998@gmail.com>
2021-06-02 23:55:36 -0500

    Agregando configuracion para maquina virtual.

---
+++ backup_virtual.sh
@@ -0,0 +1,18 @@
+sudo apt install gnome-tweaks
+sudo apt install google-chrome-stable
+sudo snap install code --classic
+sudo snap install curl
+sudo apt install git
+curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs
+sudo snap install go --classic
+sudo apt-get update
+sudo apt-get install apt-transport-https
+sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+sudo apt-get update
+sudo apt-get install dart
+sudo snap install postman
+sudo apt install postgresql postgresql-contrib
+sudo apt install pgadmin3
