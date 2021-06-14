3b8f8262824d5a4999bf3a55af257d404fa66777
Santiago Zuluaga <santiagozh1998@gmail.com>
2021-06-13 23:33:57 -0500

    Subiendo configuracion para un local server

---
+++ backup_server.sh
@@ -0,0 +1,19 @@
+sudo apt-get install update
+sudo apt-get install upgrade
+sudo snap install curl
+sudo apt-get install apt-transport-https -y
+sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+sudo apt-get install update
+sudo apt-get install dart -y
+sudo apt install net-tools -y
+sudo apt install gnome-tweaks -y
+sudo apt install git -y
+sudo snap install ngrok
+sudo snap install go --classic
+curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs -y
+sudo npm install -g yarn
+sudo apt install postgresql postgresql-contrib -y
+sudo apt-get install openssh-server -y
