f1483889dbc43327a8bca34289fd14deade271e6
Santiago Zuluaga <santiagozh1998@gmail.com>
2021-06-15 22:13:04 -0500

    Subiendo cambios

---
+++ backup_work.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install gnome-tweaks -y
+sudo snap install code --classic
+sudo snap install curl
+sudo apt install git -y
+curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs -y
+sudo apt install openjdk-8jdk -y
+sudo snap install android-studio --classic
+sudo snap install flutter --classic
+sudo snap install postman
+sudo apt install postgresql postgresql-contrib -y
+sudo apt install pgadmin3 -y
